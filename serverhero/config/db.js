d45f2c3c2b51c9aae185f55969cfff3ed63d678e
nivednivu <nivedtp6@gmail.com>
2024-09-16 14:51:24 +0530

    second message

--- serverhero/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB  = async () =>{
-    await mongoose.connect('mongodb+srv://super:super@cluster0.drklc.mongodb.net/serverhero').then(()=>{
-        console.log("db connected");
-        
-    })
-} 
